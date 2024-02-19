a0270473d5a76b9e53650aa16161a582a3090ba3
ashudes <143294793+ashudes@users.noreply.github.com>
2024-02-20 01:20:44 +0200

    Add files via upload

---
+++ OOP Module/# Python String Operations.py
@@ -0,0 +1,13 @@
+# Python String Operations
+str1 = 'Hello'
+str2 ='World!'
+
+# using +
+result = str1 + str2
+print('str1 + str2 = ', result)
+assert result == 'HelloWorld!', "Check the operation: str1 + str2"
+
+# using *
+result = str1 * 3
+print('str1 * 3 =', result)
+assert result == 'HelloHelloHello', "Check the operation: str1 * 3"
